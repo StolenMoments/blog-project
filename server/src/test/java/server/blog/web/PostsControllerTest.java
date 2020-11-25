831a75d7cb9b4cf96f9fa3cdbf5f854a1b1e5ed6
JONGHWA LEE <unknown9732@naver.com>
2020-11-25 17:41:33 +0900

    PostsController GET, POST 테스트 코드 작성

--- server/src/test/java/server/blog/web/PostsControllerTest.java
+++ server/src/test/java/server/blog/web/PostsControllerTest.java
@@ -1,10 +1,14 @@
 package server.blog.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -13,19 +17,45 @@ public class PostsControllerTest extends AbstractControllerTest {
     @Autowired
     private PostsController postsController;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private void println(String str) {
+        System.out.println(str);
+    }
+
     @Override
     protected Object controller() {
         return postsController;
     }
 
     @Test
-    public void postsGetTest() throws Exception {
-        mvc.perform(get("/api/posts/1"))
+    public void 게시글_테스트_GET() throws Exception {
+        String mvcResultString = mvc.perform(get("/api/posts/1"))
                 .andExpect(status().isOk())
-                .andDo(print());
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        println(mvcResultString);
+
     }
 
     @Test
-    public void postsPostTest() throws Exception {
+    @Transactional
+    public void 게시글_테스트_POST() throws Exception {
+        ObjectNode content = objectMapper.createObjectNode();
+        content.put("title", "제목 테스트 입니다");
+        content.put("content", "내용 테스트");
+        content.put("isNotice", 0);
+        content.put("isPrivate", 0);
+
+        mvc.perform(
+                post("/api/posts")
+                        .content(objectMapper.writeValueAsString(content))
+                        .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isOk());
+
     }
 }
